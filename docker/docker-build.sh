81cbd66fa7f4b7fa98439c2428c7e9e531ac457c
Ben Koziol <bkoziol@redlineperf.com>
2026-05-18 14:41:51 -0600

    fix spack build errors [ci skip]

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -2,9 +2,16 @@
 
 set -eux
 
+#docker buildx build \
+#  --platform linux/amd64 \
+#  -t deckyfre/catchem-spack-base:0.0.1 \
+#  -t deckyfre/catchem-spack-base:latest \
+#  -f Dockerfile-Spack-Base \
+#  .
+
 docker buildx build \
   --platform linux/amd64 \
-  -t deckyfre/catchem-spack-base:0.0.1 \
-  -t deckyfre/catchem-spack-base:latest \
-  -f Dockerfile-Spack-Base \
+  -t deckyfre/catchem-spack:0.0.1 \
+  -t deckyfre/catchem-spack:latest \
+  -f Dockerfile-Spack-CATChem \
   .
