f193ab076700ee218d11dd39e79621b60956ce80
Ben Koziol <bkoziol@redlineperf.com>
2026-05-21 13:45:45 -0600

    try docker build

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -2,15 +2,14 @@
 
 set -eux
 
-docker buildx build \
-  --platform linux/amd64 \
-  -t deckyfre/catchem-spack-base:0.0.2 \
-  -t deckyfre/catchem-spack-base:latest \
-  -f Dockerfile-Spack-Base \
-  .
+#docker buildx build \
+#  --platform linux/amd64 \
+#  -t deckyfre/catchem-spack-base:0.0.2 \
+#  -t deckyfre/catchem-spack-base:latest \
+#  -f Dockerfile-Spack-Base \
+#  .
 
 docker buildx build \
-  --no-cache \
   --platform linux/amd64 \
   -t deckyfre/catchem-spack:0.0.1 \
   -t deckyfre/catchem-spack:latest \
