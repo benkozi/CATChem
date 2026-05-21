dddbf58f36276683524c348259b6392b255e0543
Ben Koziol <bkoziol@redlineperf.com>
2026-05-21 14:58:09 -0600

    add Dockerfile [ci skip]

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -7,14 +7,14 @@ set -eux
 #  -t deckyfre/catchem-spack-base:0.0.2 \
 #  -t deckyfre/catchem-spack-base:latest \
 #  -f Dockerfile-Spack-Base \
-#  .
+#  ..
 
 #docker buildx build \
 #  --platform linux/amd64 \
 #  -t deckyfre/catchem-spack:0.0.1 \
 #  -t deckyfre/catchem-spack:latest \
 #  -f Dockerfile-Spack-CATChem \
-#  .
+#  ..
 
 docker buildx build \
   --no-cache \
@@ -22,4 +22,4 @@ docker buildx build \
   -t deckyfre/catchem:0.0.1 \
   -t deckyfre/catchem:latest \
   -f Dockerfile \
-  .
+  ..
