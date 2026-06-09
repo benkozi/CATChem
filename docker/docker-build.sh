e51e8b135245bf1caedaddbc2f1189707563412e
Ben Koziol <bkoziol@redlineperf.com>
2026-06-09 13:02:30 -0600

    ci: change repository names

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -2,12 +2,13 @@
 
 set -eux
 
-#docker buildx build \
-#  --platform linux/amd64 \
-#  -t deckyfre/catchem-spack-base:0.0.3 \
-#  -t deckyfre/catchem-spack-base:latest \
-#  -f Dockerfile-Spack-Base \
-#  ..
+docker buildx build \
+  --platform linux/amd64 \
+  -t deckyfre/catchem-spack-base-ubuntu-gcc-13-dev:latest \
+  -f Dockerfile-Spack-Base \
+  --cache-to type=inline \
+  --push \
+  ..
 
 #docker buildx build \
 #  --platform linux/amd64 \
@@ -16,10 +17,10 @@ set -eux
 #  -f Dockerfile-Spack-CATChem \
 #  ..
 
-docker buildx build \
-  --no-cache \
-  --platform linux/amd64 \
-  -t deckyfre/catchem:0.0.4 \
-  -t deckyfre/catchem:latest \
-  -f Dockerfile \
-  ..
+#docker buildx build \
+#  --no-cache \
+#  --platform linux/amd64 \
+#  -t deckyfre/catchem:0.0.4 \
+#  -t deckyfre/catchem:latest \
+#  -f Dockerfile \
+#  ..
