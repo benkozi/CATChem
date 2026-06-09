60654512a2d82fc0daf3dafeace4f112736c172a
Ben Koziol <bkoziol@redlineperf.com>
2026-06-09 13:42:35 -0600

    fix: proper ci repo

--- docker/docker-push.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-
-docker push deckyfre/catchem-spack-base:0.0.4
-docker push deckyfre/catchem-spack-base:latest
-
-docker push deckyfre/catchem:0.0.4
-docker push deckyfre/catchem:latest
